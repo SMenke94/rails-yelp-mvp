7e925732bc5e9360b06e644292ed64d55031b8b1
Saskia Menke <menkesaskia@web.de>
2018-08-08 13:58:48 +0200

    yelp app not so pretty

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: "controller#action"
+  get "restaurants", to: "restaurants#index"
+
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+
+  get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+
+  post "restaurants/:restaurant_id/reviews", to: "reviews#create", as: "restaurant_reviews"
+
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+
+  # resources :restaurants do
+  #   resources :reviews, only: [ :new, :create ]
+  # end
 end
